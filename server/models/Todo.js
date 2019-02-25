e3f8ee87f02df56d8f3a19de2ed95cad6ef5b8ac
Lego <qas612820704@gmail.com>
2019-02-25 11:27:55 +0800

    Give 'null' as default value of todo.completeAt, let redux easier to handle activation

--- server/models/Todo.js
+++ server/models/Todo.js
@@ -2,7 +2,7 @@ import { model, Schema } from 'mongoose';
 
 const Todo = new Schema({
   message: String,
-  completedAt: Date,
+  completedAt: { type: Date, default: null },
 }, {
   // this will add createdAt and updatedAt timestamp when save
   timestamps: true,
